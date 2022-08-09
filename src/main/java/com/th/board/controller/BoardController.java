82cc22ffc4ce5d0cb03608141a17d1e8ef187be7
dhy02094 <dhy02074@naver.com>
2022-08-09 16:52:40 +0900

    12

--- src/main/java/com/th/board/controller/BoardController.java
+++ src/main/java/com/th/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.th.board.dto.BoardDto;
 import com.th.board.service.BoardService;
@@ -72,6 +73,14 @@ public class BoardController {
     public String delete(@PathVariable("no") Long id) {
         boardService.deletePost(id);
         return "redirect:/";
+    }
+
+    @GetMapping("/board/search")
+    public String search(@RequestParam(value = "keyword") String keyword, Model model){
+        List<BoardDto> boardDtoList = boardService.searchPosts(keyword);
+        model.addAttribute("boardList", boardDtoList);
+
+        return "board/list.html";
     }
 
 
--- src/main/java/com/th/board/repository/BoardRepository.java
+++ src/main/java/com/th/board/repository/BoardRepository.java
@@ -1,9 +1,11 @@
 package com.th.board.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.th.board.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long>{
-    
+    List<Board> findByTitleContaining(String keyword);
 }
